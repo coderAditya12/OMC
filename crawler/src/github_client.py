e30bc8b9b297366ecd4c5c845ad162107d2f2346
Aditya Gupta <realadityagupta2@gmail.com>
2025-12-25 15:21:59 +0530

    github issues api written

---
+++ crawler/src/github_client.py
@@ -0,0 +1,43 @@
+import httpx
+from typing import List, Dict, Any
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class GithubClient:
+    def __init__(self):
+        self.token = os.getenv("GITHUB_ACCESS_TOKEN")
+        if not self.token:
+            raise ValueError("GITHUB_ACCESS_TOKEN not found in dotenv file")
+        self.base_url = "https://api.github.com/"
+        self.headers = {
+            "Authorization": f"token {self.token}",
+            "Accept": "application/vnd.github+json",
+            "User-Agent": "OSM-Crawler-v1",
+        }
+
+    def fetch_github_issues(self, owner: str, repo: str, label: str = "bug"):
+        # fetch issue from github api with specific repo
+        url = f"{self.base_url}repos/{owner}/{repo}/issues"
+
+        params = {
+            "accept": "application/vnd.github+json",
+            "state": "open",
+            "labels": label,
+            "sort": "updated",
+            "direction": "desc",
+            "per_page": 10,
+        }
+        print(f"📡 Connecting to GitHub: {owner}/{repo} looking for '{label}'...")
+        try:
+            with httpx.Client() as client:
+                response = client.get(url, headers=self.headers, params=params)
+                response.raise_for_status()
+                return response.json()
+        except httpx.HTTPStatusError as e:
+            print(f"HTTP ERROR {e.response.status_code} - {e.response.text}")
+        except Exception as e:
+            print(f"connection error: {e}")
+            return []
---
+++ crawler/src/main.py
@@ -0,0 +1,44 @@
+import json
+import os
+from github_client import GithubClient
+OUTPUT_DIR = "./output"
+os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+def run_crawler():
+    client = GithubClient()
+    target_owner = "MemoriLabs"
+    target_repo = "Memori"
+    raw_issues = client.fetch_github_issues(target_owner, target_repo)
+    if not raw_issues:
+        print("no issues found or access denied.")
+        return
+    # clean the data
+    cleaned_issues = []
+    for issue in raw_issues:
+        if "pull_request" in issue:
+            continue
+        cleaned_issue = {
+            "id": issue["id"],
+            "state": issue["state"],
+            "title": issue["title"],
+            "url": issue["html_url"],
+            "labels": [label["name"] for label in issue["labels"]],
+            "body_summary": issue["body"][:200] + "..."
+            if issue["body"]
+            else "No description",
+
+        }
+        cleaned_issues.append(cleaned_issue)
+    print(cleaned_issues)    
+    
+    output_file = f"{OUTPUT_DIR}/{target_repo}_issues.json"
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(cleaned_issues, f, indent=4)
+
+    print(f"✅ Success! Scraped {len(cleaned_issues)} issues.")
+    print(f"📁 Data saved to: {output_file}")
+
+
+if __name__ == "__main__":
+    run_crawler()
+    
