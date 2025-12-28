075c894fd66aaa5ed5ee13a0bc36a8d934d455d4
Aditya Gupta <realadityagupta2@gmail.com>
2025-12-28 11:22:07 +0530

    rag phase 1 completed

---
+++ crawler/src/config.py
@@ -0,0 +1,5 @@
+from dotenv import load_dotenv
+import os
+load_dotenv()
+pinecone_key = os.getenv("PINECONE_API_KEY")
+index = os.getenv("PINECONE_INDEX")
--- crawler/src/vector_store.py
+++ crawler/src/vector_store.py
@@ -1,17 +1,43 @@
 from pinecone import Pinecone
 from dotenv import load_dotenv
 from langchain_pinecone import PineconeVectorStore
-
+from config import pinecone_key, index
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-import os
 
 load_dotenv()
-pinecone_key = os.getenv("PINECONE_API_KEY")
-pinecone_index = os.getenv("PINECONE_INDEX")
 # embedding model
-embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
-index = pinecone_key.index(pinecone_index)
 
-vector_store = PineconeVectorStore(embedding=embeddings, index=pinecone_index)
 
-pc = Pinecone
+class vectorDB:
+    def __init__(self):
+        self.pc = Pinecone(pinecone_key)
+        self.embeddings = GoogleGenerativeAIEmbeddings(
+            model="models/text-embedding-004"
+        )
+        self.index = self.pc.Index(index)
+        self.vector_store = PineconeVectorStore(embedding=self.embeddings, index=index)
+
+    def upsert_issues(self, issues):
+        """
+        Takes a list of Issue dictionaries (or objects), creates embeddings,
+        and uploads to Pinecone.
+        """
+        print(f"🧠 Generating embeddings for {len(issues)} issues...")
+        vector_to_upload = []
+        for issue in issues:
+            text_to_embed = f"Title:{issue.title}\nBody:{issue.body or ''}"
+            generate_embeddings = self.embeddings.aembed_query(text_to_embed)
+            vector_to_upload.append(
+                {
+                    "id": str(issue.github_issue_id),
+                    "values": generate_embeddings,
+                    "metadata": {
+                        "url": issue.url,
+                        "title": issue.title,
+                        "repo": issue.repo_name,
+                    },
+                }
+            )
+        if vector_to_upload:
+            self.index.upsert(vectors=vector_to_upload)
+            print(f"Successfully uploaded {len(vector_to_upload)} vectors to pinecone")
