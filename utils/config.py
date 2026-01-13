f259d424a349dfff9c158b479bf8460121ce898b
aditya <realadityagupta2@gmail.com>
2026-01-13 13:58:50 +0530

    db changed

--- utils/config.py
+++ utils/config.py
@@ -3,7 +3,7 @@ import os
 load_dotenv()
 pinecone_key = os.getenv("PINECONE_API_KEY")
 index = os.getenv("PINECONE_INDEX")
-DB_URL = os.getenv("DATABASE_URL")
+DB_URL =os.getenv("DATABASE_URL")
 print(DB_URL)
 GITHUB_ACCESS_TOKEN = os.getenv("GITHUB_ACCESS_TOKEN")
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
